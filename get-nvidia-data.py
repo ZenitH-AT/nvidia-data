5f13f1ab349634a788236af52dd377fc66971a84
Robert Berge <31069197+ZenitH-AT@users.noreply.github.com>
2021-06-06 20:54:59 +0200

    use tabs instead of spaces

--- get-nvidia-data.py
+++ get-nvidia-data.py
@@ -7,27 +7,27 @@ CARD_VARIANT_REGEX = r".*(?= [0-9]+GB)|.*(?= \([A-Z]+\))|.*"
 NOTEBOOK_SERIES_REGEX = r".*((Notebooks)|Quadro Blade).*"
 
 def get_lookup_values(type_id):
-    xml = requests.get(f"https://www.nvidia.com/Download/API/lookupValueSearch.aspx?TypeID={type_id}").content
+	xml = requests.get(f"https://www.nvidia.com/Download/API/lookupValueSearch.aspx?TypeID={type_id}").content
 
-    lookup_values = xmltodict.parse(xml)["LookupValueSearch"]["LookupValues"]["LookupValue"]
+	lookup_values = xmltodict.parse(xml)["LookupValueSearch"]["LookupValues"]["LookupValue"]
 
-    return lookup_values
+	return lookup_values
 
 def clean_gpu_name(gpu_name):
-    # Limit to first name if multiple names specified; most alternative names are repeated in other entries
-    # (e.g. "GeForce 7050 / NVIDIA nForce 610i" is repeated as "nForce 610i/GeForce 7050")
-    gpu_name = str(gpu_name).replace("NVIDIA", "").split("/")[0].strip()
+	# Limit to first name if multiple names specified; most alternative names are repeated in other entries
+	# (e.g. "GeForce 7050 / NVIDIA nForce 610i" is repeated as "nForce 610i/GeForce 7050")
+	gpu_name = str(gpu_name).replace("NVIDIA", "").split("/")[0].strip()
 
-    # Handle card variants (e.g. 1060 6GB, 760Ti (OEM))
-    gpu_name = re.match(CARD_VARIANT_REGEX, gpu_name)[0]
+	# Handle card variants (e.g. 1060 6GB, 760Ti (OEM))
+	gpu_name = re.match(CARD_VARIANT_REGEX, gpu_name)[0]
 
-    return gpu_name
+	return gpu_name
 
 def write_json(data, file_name):
-    json_object = json.dumps(data, indent=4)
+	json_object = json.dumps(data, indent=4)
 
-    with open(file_name, "w+") as outfile:
-        outfile.write(json_object)
+	with open(file_name, "w+") as outfile:
+		outfile.write(json_object)
 
 ## Parse GPUs
 series_lookup_values = get_lookup_values(2)
@@ -40,12 +40,12 @@ gpu_lookup_values = get_lookup_values(3)
 gpu_dict = {"desktop": {}, "notebook": {}}
 
 for gpu_lookup_value in gpu_lookup_values:
-    gpu_pair = {clean_gpu_name(gpu_lookup_value["Name"]): gpu_lookup_value["Value"]}
+	gpu_pair = {clean_gpu_name(gpu_lookup_value["Name"]): gpu_lookup_value["Value"]}
 
-    if gpu_lookup_value["@ParentID"] in notebook_series_values:
-        gpu_dict["notebook"].update(gpu_pair)
-    else:
-        gpu_dict["desktop"].update(gpu_pair)
+	if gpu_lookup_value["@ParentID"] in notebook_series_values:
+		gpu_dict["notebook"].update(gpu_pair)
+	else:
+		gpu_dict["desktop"].update(gpu_pair)
 
 write_json(gpu_dict, "gpu-data.json")
 
